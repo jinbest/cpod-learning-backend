94fac1b925c43bcd0ebb560f18d7b084f3783cca
gumball <ugis.rozkalns@gmail.com>
2019-06-27 18:39:57 +0800

    Production settings

---
+++ api/controllers/request.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'Request Parameter dump',
+
+
+
+  exits: {
+
+    success: {
+      description: 'New user account was created successfully.'
+    },
+
+    invalid: {
+      responseType: 'badRequest',
+      description: 'The provided fullName, password and/or email address are invalid.',
+      extendedDescription: 'If this request was sent from a graphical user interface, the request '+
+        'parameters should have been validated/coerced _before_ they were sent.'
+    },
+
+  },
+
+  fn: async function (inputs) {
+    var req = this.req;
+    return req.ip;
+  }
+};
