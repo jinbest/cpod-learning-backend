f7d2cf8ff69bef8d771005c548f6e4adf5fb2f39
gumball <ugis.rozkalns@gmail.com>
2019-07-04 19:01:07 +0800

    Adding CodeDeploy configs

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,7 +2,7 @@
 
 # Install node.js
 sudo apt-get install python-software-properties -y
-sudo apt-add-repository ppa:chris-lea/node.js -y
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get update
 sudo apt-get install nodejs -y
 
