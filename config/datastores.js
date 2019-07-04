6df68d540c180fd85de9611ec29e944d9ca50b03
gumball <ugis.rozkalns@gmail.com>
2019-07-04 11:05:44 +0800

    Cloud cleanup

--- config/datastores.js
+++ config/datastores.js
@@ -33,25 +33,13 @@ module.exports.datastores = {
   ***************************************************************************/
 
   default: {
-
-    /***************************************************************************
-    *                                                                          *
-    * Want to use a different database during development?                     *
-    *                                                                          *
-    * 1. Choose an adapter:                                                    *
-    *    https://sailsjs.com/plugins/databases                                 *
-    *                                                                          *
-    * 2. Install it as a dependency of your Sails app.                         *
-    *    (For example:  npm install sails-mysql --save)                        *
-    *                                                                          *
-    * 3. Then pass it in, along with a connection URL.                         *
-    *    (See https://sailsjs.com/config/datastores for help.)                 *
-    *                                                                          *
-    ***************************************************************************/
     adapter: 'sails-mysql',
     url: 'mysql://cpoddba:fr0t3ll@@cpodjsweb.cx6o0r5nidjs.us-east-1.rds.amazonaws.com:3306/chinesepod_production',
-
   },
+  // mongoDB: {
+  //   adapter: 'sails-mongo',
+  //   url: 'mongodb://cpoddba:fr0t3ll@@cpod-production.cluster-cx6o0r5nidjs.us-east-1.docdb.amazonaws.com',
+  // }
 
 
 };
