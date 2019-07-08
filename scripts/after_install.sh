3ab2073bd6269e7ec70a5d8d2cfa6977c098f9fb
gumball <ugis.rozkalns@gmail.com>
2019-07-08 17:25:24 +0800

    Adding CodeDeploy configs

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,7 +3,9 @@
 mkdir /home/ubuntu/nodejs
 cd /home/ubuntu/nodejs
 
+# Install dependancies
 sudo npm install
 
+# Setup nginx
 sudo cp /home/ubuntu/nodejs/scripts/nginx /etc/nginx/sites-available/default
 sudo systemctl restart nginx
