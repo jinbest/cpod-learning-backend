89645d4cc65322f9ec294e825dbe3bbff390d03c
gumball <ugis.rozkalns@gmail.com>
2019-07-08 18:22:28 +0800

    Adding CodeDeploy configs

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-mkdir /home/ubuntu/nodejs
 cd /home/ubuntu/nodejs
 
-# Install dependancies
+# Install dependencies
 sudo npm install
+
+sudo pm2 start /home/ubuntu/nodejs/app.js --wait-ready --listen-timeout 10000 -- --prod
 
 # Setup nginx
 sudo cp /home/ubuntu/nodejs/scripts/nginx /etc/nginx/sites-available/default
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Stop all servers and start the server as a daemon
-pm2 stop all
-sudo pm2 start /home/ubuntu/nodejs/app.js -i 0 -- --prod
+sudo pm2 stop all
+pm2 start /home/ubuntu/nodejs/app.js -i 0 -- --prod
 pm2 save
