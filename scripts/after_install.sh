1837a75e9455a9d821d2593659a8bb4927eb871f
gumball <ugis.rozkalns@gmail.com>
2019-07-12 15:53:44 +0800

    Cleanup

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,8 +5,8 @@ cd /home/ubuntu/nodejs
 # Install dependencies
 sudo npm install
 
-# sudo pm2 start /home/ubuntu/nodejs/app.js --wait-ready --listen-timeout 10000 -- --prod
+# sudo pm2 start /home/ubuntu/chinesepod.com/app.js --wait-ready --listen-timeout 10000 -- --prod
 
 # Setup nginx
-sudo cp /home/ubuntu/nodejs/scripts/nginx /etc/nginx/sites-available/default
+sudo cp /home/ubuntu/chinesepod.com/scripts/nginx /etc/nginx/sites-available/default
 sudo systemctl restart nginx
