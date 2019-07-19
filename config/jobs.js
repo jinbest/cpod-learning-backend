b357f90479db91433cb4ebb2e31f0250613bed45
gumball <ugis.rozkalns@gmail.com>
2019-07-19 17:55:15 +0800

    Reworking Lesson Recap Endpoint

---
+++ config/jobs.js
@@ -0,0 +1,10 @@
+/**
+ * Default jobs configuration
+ * (sails.config.jobs)
+ *
+ * For more information using jobs in your app, check out:
+ * https://github.com/vbuzzano/sails-hook-jobs
+ */
+module.exports.jobs = {
+  url: 'redis://cpod-production.idthgn.ng.0001.use1.cache.amazonaws.com:6379/8'
+};
