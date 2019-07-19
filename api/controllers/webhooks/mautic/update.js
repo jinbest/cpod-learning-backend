336f136258755124eab7807c112c8ee125f049b1
gumball <ugis.rozkalns@gmail.com>
2019-07-19 18:32:19 +0800

    Reworking Lesson Recap Endpoint

--- api/controllers/webhooks/mautic/update.js
+++ api/controllers/webhooks/mautic/update.js
@@ -31,14 +31,13 @@ module.exports = {
   fn: async function (inputs) {
     let email = '';
     try {
-      sails.log(this.req.body);
       email = this.req.body['mautic.lead_post_save_new'][0].contact.fields.core.email.value;
     } catch (e) {
       sails.log('Invalid Webhook Event');
     }
     if (email === '' && inputs.email) {
       email = inputs.email;
-    } else {
+    } else if (email === '') {
       throw 'invalid'
     }
     var Queue = require('bull');
