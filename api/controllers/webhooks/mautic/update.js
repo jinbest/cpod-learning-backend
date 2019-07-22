c7e21eb5178477afb2b9138856e3ab7ee27e3a5c
gumball <ugis.rozkalns@gmail.com>
2019-07-22 16:34:07 +0800

    Mautic webhook

--- api/controllers/webhooks/mautic/update.js
+++ api/controllers/webhooks/mautic/update.js
@@ -31,6 +31,7 @@ module.exports = {
   fn: async function (inputs) {
     let email = '';
     try {
+      sails.log(this.req.body);
       email = this.req.body[0][0].contact.fields.core.email.value;
     } catch (e) {
       sails.log('Invalid Webhook Event');
