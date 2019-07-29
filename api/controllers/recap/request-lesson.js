392490d2d070ba0975619aa0bbeb2c5caaafbaba
gumball <ugis.rozkalns@gmail.com>
2019-07-29 12:35:10 +0800

    Adding Recap Request mechanic.

--- api/controllers/recap/request-lesson.js
+++ api/controllers/recap/request-lesson.js
@@ -37,11 +37,26 @@ module.exports = {
 
 
   fn: async function (inputs) {
-    if (sessionId) {
-      await sails.helpers.phpApi.checkSession(inputs.sessionId);
+    if (!inputs.sessionId) {
+      throw 'invalid'
     }
 
-    return;
+    let session = await sails.helpers.phpApi.checkSession(inputs.sessionId);
+
+    let userData = await User.findOne({email:session.email});
+
+    await sails.helpers.sendTemplateEmail.with({
+      to: 'ugis@chinesepod.com', //'recap-request@chinesepod.com',
+      subject: 'Recap Lesson Request',
+      template: 'internal/email-notification',
+      layout: false,
+      templateData: {
+        contactName: userData.name,
+        contactEmail: userData.email,
+        topic: 'Requesting a Recap Lesson', //inputs.topic,
+        message: 'Some message' //inputs.message
+      }
+    });
 
   }
 
