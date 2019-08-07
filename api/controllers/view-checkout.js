cfe535498e56aa9110bd369b072e48c443370f69
gumball <ugis.rozkalns@gmail.com>
2019-08-07 18:39:02 +0800

    Initial work on Stripe - including errors

--- api/controllers/view-checkout.js
+++ api/controllers/view-checkout.js
@@ -17,30 +17,40 @@ module.exports = {
 
   fn: async function () {
 
-    let trial = false; let promo = false; let plan = false; let promoCode = '';
-    if(this.req.param('trial', false) || this.req.session.trial ) {
-      trial = true
-    }
+    let trial = false; let promo = false; let plan = 'premium'; let period = 'monthly'; let promoCode = '';
+
     if(this.req.param('promo', false)) {
       promo = true;
       if (this.req.param('promo')) {
-        promoCode = this.req.param('promo') == 'yes' ? '' : this.req.param('promo')
+        promoCode = this.req.param('promo').toLowerCase() === 'yes' ? '' : this.req.param('promo').toUpperCase();
       }
     }
-
+    if(this.req.param('trial', false) || this.req.session.trial ) {
+      trial = true;
+      promo = false;
+      promoCode = '';
+    }
+    if(this.req.param('plan', false) && ['premium', 'basic'].includes(this.req.param('plan', false).toLowerCase())) {
+      plan = this.req.param('plan', false).toLowerCase();
+    }
+    if(this.req.param('period', false) && ['annually', 'quarterly', 'monthly'].includes(this.req.param('period', false).toLowerCase())) {
+      period = this.req.param('period', false).toLowerCase();
+    }
+    sails.log.info({
+      plan: this.req.param('plan', false),
+      period: this.req.param('period', false)
+    });
 
     // Respond with view.
     return {
       needsAccount: !this.req.session.userId,
       trial: trial,
+      plan: plan,
+      billingCycle: period,
       promoShow: promo,
       formData: {
         promoCode: promoCode
       }
     };
-
-
   }
-
-
 };
