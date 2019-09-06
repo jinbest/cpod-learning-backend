5daed12694f9fa231f9e0e18785489aa5da9d0d7
gumball <ugis.rozkalns@gmail.com>
2019-09-06 11:05:49 +0800

    Adding Recap App Page

--- config/routes.js
+++ config/routes.js
@@ -56,7 +56,8 @@ module.exports.routes = {
   'GET /secret-page':             { action: 'recap/view-secret-page' },
   'GET /current-lesson':          { action: 'recap/view-current-lesson' },
   'GET /set-lesson/:unused?':     { action: 'recap/set-lesson' },
-
+  'GET /recap':                   { action: 'recap/view-recap' },
+  'GET /recapp':                  { action: 'recap/view-recap' },
 
 
   //  ╔╦╗╦╔═╗╔═╗  ╦═╗╔═╗╔╦╗╦╦═╗╔═╗╔═╗╔╦╗╔═╗   ┬   ╔╦╗╔═╗╦ ╦╔╗╔╦  ╔═╗╔═╗╔╦╗╔═╗
