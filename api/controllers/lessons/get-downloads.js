4ae20ba1eaac5fde7bc7087b8b6502607e4ffd1d
gumball <ugis.rozkalns@gmail.com>
2019-09-20 12:51:00 +0800

    Work on APIs

--- api/controllers/lessons/get-downloads.js
+++ api/controllers/lessons/get-downloads.js
@@ -33,20 +33,21 @@ module.exports = {
         type: 'premium',
         downloads: {}
       };
+      let extra = lessonData.type === 'extra';
       if (lessonData.mp3_private) {
-        returnData.downloads.lesson = 'https://s3contents.chinesepod.com/' + lessonData.id + '/' + lessonData.hash_code + '/' + lessonData.mp3_private;
+        returnData.downloads.lesson = 'https://s3contents.chinesepod.com/' + (extra ? 'extra/' : '') + lessonData.id + '/' + lessonData.hash_code + '/' + lessonData.mp3_private;
       }
       if (lessonData.mp3_dialogue) {
-        returnData.downloads.dialogue = 'https://s3contents.chinesepod.com/' + lessonData.id + '/' + lessonData.hash_code + '/' + lessonData.mp3_dialogue;
+        returnData.downloads.dialogue = 'https://s3contents.chinesepod.com/' + (extra ? 'extra/' : '') + lessonData.id + '/' + lessonData.hash_code + '/' + lessonData.mp3_dialogue;
       }
       if (lessonData.mp3_thefix) {
-        returnData.downloads.review = 'https://s3contents.chinesepod.com/' + lessonData.id + '/' + lessonData.hash_code + '/' + lessonData.mp3_thefix;
+        returnData.downloads.review = 'https://s3contents.chinesepod.com/' + (extra ? 'extra/' : '') + lessonData.id + '/' + lessonData.hash_code + '/' + lessonData.mp3_thefix;
       }
       if (lessonData.pdf1) {
-        returnData.downloads.pdf1 = 'https://s3contents.chinesepod.com/' + lessonData.id + '/' + lessonData.hash_code + '/' + lessonData.pdf1;
+        returnData.downloads.pdf1 = 'https://s3contents.chinesepod.com/' + (extra ? 'extra/' : '') + lessonData.id + '/' + lessonData.hash_code + '/' + lessonData.pdf1;
       }
       if (lessonData.pdf2) {
-        returnData.downloads.pdf2 = 'https://s3contents.chinesepod.com/' + lessonData.id + '/' + lessonData.hash_code + '/' + lessonData.pdf2;
+        returnData.downloads.pdf2 = 'https://s3contents.chinesepod.com/' + (extra ? 'extra/' : '') + lessonData.id + '/' + lessonData.hash_code + '/' + lessonData.pdf2;
       }
       return returnData;
     } else {
