b61e3300aece32d145a11938b7296eaa779667b3
gumball <ugis.rozkalns@gmail.com>
2019-10-14 16:50:22 +0800

    Exercises

--- api/controllers/exercises/results/get.js
+++ api/controllers/exercises/results/get.js
@@ -27,8 +27,14 @@ module.exports = {
     inputs.userId = sails.config.environment === 'development' ? 1016995 : this.req.session.userId;
 
     // All done.
-    return await UserAssessments.find({user_id: inputs.userId, lesson_id: inputs.lessonId});
+    let assessments = await UserAssessments
+      .find({user_id: inputs.userId, lesson_id: inputs.lessonId})
+      .sort('createdAt DESC');
 
+    return {
+      count: assessments.length,
+      results: assessments
+    }
   }
 
 
--- api/controllers/exercises/results/post.js
+++ api/controllers/exercises/results/post.js
@@ -14,21 +14,44 @@ module.exports = {
     },
     userId: {
       type: 'number'
+    },
+    totalScore: {
+      type: 'number',
+      required: true
+    },
+    userScore: {
+      type: 'number',
+      required: true
     }
   },
 
 
   exits: {
-
+    invalid: {
+      responseType: 'badRequest'
+    }
   },
 
 
   fn: async function (inputs) {
     inputs.userId = sails.config.environment === 'development' ? 1016995 : this.req.session.userId;
 
-    // All done.
-    return;
+    if (!inputs.userId) {
+      throw 'invalid'
+    }
 
+    let assessment = (await Assessments.find({product_id: 1, v3_id: inputs.lessonId}).limit(1))[0];
+
+    // All done.
+    return await UserAssessments.create({
+      user_id: inputs.userId,
+      assessment_id: assessment.id,
+      lesson_id: inputs.lessonId,
+      assessment_name: assessment.name,
+      total_score: inputs.totalScore,
+      user_score: inputs.userScore,
+      created_by: inputs.userId
+    })
   }
 
 
