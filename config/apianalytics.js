ff8eac9ae7504cfa012e919a1500f5a29402b2d7
gumball <ugis.rozkalns@gmail.com>
2019-10-17 17:08:16 +0800

    Logging work

--- config/apianalytics.js
+++ config/apianalytics.js
@@ -4,22 +4,21 @@ module.exports.apianalytics = {
       '/api/v1/health/time'
     ];
 
-    if(ignore.includes(req.path)) {
-      return
+    if(!ignore.includes(req.path)) {
+      sails.hooks.jobs.loggingQueue.add('Logging Requests',
+        {
+          userId: req.session.userId,
+          ip: req.ip,
+          url: `https://www.chinesepod.com${req.url}`,
+          sessionId: req.session.id,
+          urlbase: `https://www.chinesepod.com${req.path}`,
+          referer: req.get('referer')
+        },
+        {
+          attempts: 2,
+          timeout: 60000
+        }
+      );
     }
-
-    sails.hooks.jobs.loggingQueue.add('Logging Requests',
-      {
-        userId: req.session.userId,
-        ip: req.ip,
-        url: `https://www.chinesepod.com${req.url}`,
-        sessionId: req.session.id,
-        urlbase: `https://www.chinesepod.com${req.path}`,
-        referer: req.get('referer')
-      },
-      {
-        attempts: 2,
-        timeout: 20000
-      });
   },
 };
