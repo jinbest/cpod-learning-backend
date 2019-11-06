317d8feb740a169b8ddd1b6b8089dfbe9958fecf
gumball <ugis.rozkalns@gmail.com>
2019-11-06 12:50:07 +0800

    Dashboard Update

---
+++ assets/dash/precache-manifest.f9be31c1c0408ce690575907ba32dbdd.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/dash/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "fde9f13263319678c5c71510391e2a85",
+    "url": "/dash/index.html"
+  },
+  {
+    "revision": "35f17eaaad709f8c92c6",
+    "url": "/dash/js/chunk-490e6dd7-legacy.00c80fb9.js"
+  },
+  {
+    "revision": "06922478364dc3af4245",
+    "url": "/dash/js/chunk-ab7e0cd8-legacy.ceaa0eeb.js"
+  },
+  {
+    "revision": "90573f597682951a9176",
+    "url": "/dash/js/app-legacy.96f2dcf4.js"
+  },
+  {
+    "revision": "48849af14578c1d45756",
+    "url": "/dash/js/chunk-vendors-legacy.fe5047f7.js"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/dash/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "a070ebf69a7fcaea3c17fbbc56deebac",
+    "url": "/dash/img/logo.a070ebf6.png"
+  },
+  {
+    "revision": "4bc57bd7c9fd6fe19a5cba5c2b57b0fb",
+    "url": "/dash/img/preloader-square.4bc57bd7.svg"
+  },
+  {
+    "revision": "d7c26c3af7979c14ed3da333f9de6e59",
+    "url": "/dash/img/symbol-black-center.d7c26c3a.svg"
+  },
+  {
+    "revision": "e4b6e6c5749ebe66252e0da478f72b5b",
+    "url": "/dash/img/qing-wen.e4b6e6c5.jpg"
+  },
+  {
+    "revision": "f9b78d93af8ccff72e6e853d649a19cd",
+    "url": "/dash/img/symbol.f9b78d93.svg"
+  },
+  {
+    "revision": "92d40536b4008c9e966baf28829ce412",
+    "url": "/dash/img/preloader.92d40536.svg"
+  },
+  {
+    "revision": "eee8b9b975b37eef73cda9641feb8f6f",
+    "url": "/dash/fonts/icomoon.eee8b9b9.woff"
+  },
+  {
+    "revision": "48849af14578c1d45756",
+    "url": "/dash/css/chunk-vendors.5e70e794.css"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/dash/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/dash/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "e9c8d6429bab307155971201fd98217b",
+    "url": "/dash/fonts/icomoon.e9c8d642.ttf"
+  },
+  {
+    "revision": "00d2977773069da3cd07cfca2c2482e6",
+    "url": "/dash/fonts/icomoon.00d29777.eot"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/dash/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "d9cfd664237d1473d68127968d0f3776",
+    "url": "/dash/img/icomoon.d9cfd664.svg"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/dash/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "61f77c5e336a55fb8dc944a0676c5f5b",
+    "url": "/dash/img/brand.61f77c5e.svg"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/dash/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "365fc333c183bf39ef777a760ca718cf",
+    "url": "/dash/img/brand-white.365fc333.svg"
+  },
+  {
+    "revision": "90573f597682951a9176",
+    "url": "/dash/css/app.3d5da9a0.css"
+  }
+];
--- assets/dash/service-worker.js
+++ assets/dash/service-worker.js
@@ -15,7 +15,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 
 importScripts(
   "/dash/updateSW.js",
-  "/dash/precache-manifest.ffe1f08c8937d405e11f611920ccc6de.js"
+  "/dash/precache-manifest.c185a56f002d10be8941d397bee53cc4.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "chinesepod-dashboard"});
