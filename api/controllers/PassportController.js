d8a0da63be79dd4fac718875ef83bf5d898eb8b7
gumball <ugis.rozkalns@gmail.com>
2019-11-07 12:57:39 +0800

    Adding Auths

--- api/controllers/PassportController.js
+++ api/controllers/PassportController.js
@@ -7,17 +7,80 @@
 
 var passport = require('passport');
 
+var ipdata = require('ipdata');
+
 module.exports = {
   facebookAuth: function(req, res, next) {
     passport.authenticate('facebook', { scope: ['email']})(req, res, next);
   },
 
   facebookCallback: function(req, res, next) {
-    passport.authenticate('facebook', function(err, user) {
+    passport.authenticate('facebook', async function(err, user) {
 
-      console.log('facebook credentials');
-      console.log(user);
-      res.json(user);
+      if(err) {
+
+        // redirect to login page
+        sails.log.error('facebook callback error: '+err);
+        res.redirect('/login');
+
+      } else {
+
+        sails.log.info('facebook credentials');
+        sails.log.info(user);
+
+        let userData = await User.findOne({email: user.email.toLowerCase()});
+
+        if (!userData) {
+          const ip = req.ip ? req.ip : false;
+          let ipData = {};
+
+          if(ip && ip !== '::1') {
+            await ipdata.lookup(req.ip, sails.config.custom.ipDataKey)
+              .then((info) => {
+                ipData = info;
+              })
+              .catch((err) => {
+                sails.log.error(err);
+              });
+          }
+
+          user.name = user.first_name + ' ' + user.last_name
+
+          userData = await User.create({
+            email: user.email,
+            password: user.id,
+            username: user.name.split(' ').join('') + Math.ceil(Math.random() * 100),
+            name: user.name,
+            ip_address: ip,
+            ip_country: ipData['country_name'] ? ipData['country_name'] : null,
+            ip_region: ipData['region'] ? ipData['region'] : null,
+            ip_city: ipData['city'] ? ipData['city'] : null,
+            country: ipData['country_name'] ? ipData['country_name'] : null,
+            city: ipData['city'] ? ipData['city'] : null,
+            http_referer: req.headers.referer ? req.headers.referer : '',
+            code: user.id,
+            confirm_status: 1
+          }).fetch();
+        }
+
+        // Modify the active session instance.
+        // (This will be persisted when the response is sent.)
+        req.session.userId = userData.id;
+
+        await sails.helpers.createPhpSession.with({
+          userId: userData.id,
+        })
+          .then((phpSessionId) => {
+            res.cookie('CPODSESSID', phpSessionId, {
+              domain: '.chinesepod.com',
+              expires: new Date(Date.now() + 365.25 * 24 * 60 * 60 * 1000)
+            });
+          });
+
+        res.redirect('/dash')
+
+
+      }
     })(req, res, next);
   },
 
@@ -26,14 +89,68 @@ module.exports = {
   },
 
   googleCallback: function(req, res, next) {
-    passport.authenticate('google', function(err, user) {
+
+    passport.authenticate('google', async function(err, user) {
       if(err) {
+
         // redirect to login page
-        console.log('google callback error: '+err);
+        sails.log.error('google callback error: '+err);
+        res.redirect('/login');
+
       } else {
-        console.log('google credentials');
-        console.log(user);
-        res.json(user);
+
+        sails.log.info('google credentials');
+        sails.log.info(user);
+
+        let userData = await User.findOne({email: user.email.toLowerCase()});
+
+        if (!userData) {
+          const ip = req.ip ? req.ip : false;
+          let ipData = {};
+
+          if(ip && ip !== '::1') {
+            await ipdata.lookup(req.ip, sails.config.custom.ipDataKey)
+              .then((info) => {
+                ipData = info;
+              })
+              .catch((err) => {
+                sails.log.error(err);
+              });
+          }
+
+          userData = await User.create({
+            email: user.email,
+            password: user.id,
+            username: user.name.split(' ').join('') + Math.ceil(Math.random() * 100),
+            name: user.name,
+            ip_address: ip,
+            ip_country: ipData['country_name'] ? ipData['country_name'] : null,
+            ip_region: ipData['region'] ? ipData['region'] : null,
+            ip_city: ipData['city'] ? ipData['city'] : null,
+            country: ipData['country_name'] ? ipData['country_name'] : null,
+            city: ipData['city'] ? ipData['city'] : null,
+            http_referer: req.headers.referer ? req.headers.referer : '',
+            code: user.id,
+            confirm_status: 1
+          }).fetch();
+        }
+
+        // Modify the active session instance.
+        // (This will be persisted when the response is sent.)
+        req.session.userId = userData.id;
+
+        await sails.helpers.createPhpSession.with({
+          userId: userData.id,
+        })
+          .then((phpSessionId) => {
+            res.cookie('CPODSESSID', phpSessionId, {
+              domain: '.chinesepod.com',
+              expires: new Date(Date.now() + 365.25 * 24 * 60 * 60 * 1000)
+            });
+          });
+
+        res.redirect('/dash')
+
       }
     })(req, res, next);
   },
