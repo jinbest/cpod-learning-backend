0987293e1569e18baa650ba3d518341322e4a995
gumball <ugis.rozkalns@gmail.com>
2019-11-07 14:24:45 +0800

    Adding Auths

--- config/env/staging.js
+++ config/env/staging.js
@@ -74,9 +74,15 @@ module.exports = Object.assign({}, PRODUCTION_CONFIG, {
 
   http: {
     middleware: {
+
+      passportInit: require('passport').initialize(),
+      passportSession: require('passport').session(),
+
       order: [
         'cookieParser',
         'session',
+        'passportInit',
+        'passportSession',
         'bodyParser',
         'compress',
         'poweredBy',
