8d05863300f23f9a953ccf393a0d620a749a4b7a
gumball <ugis.rozkalns@gmail.com>
2019-11-12 15:13:17 +0800

    Adding dashboard cookie

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -9,6 +9,11 @@ module.exports = {
   serve: (req, res) => {
     const fs = require('fs');
 
+    this.res.cookie('new_dashboard', true, {
+      domain: '.chinesepod.com',
+      expires: new Date(Date.now() + 365.25 * 24 * 60 * 60 * 1000)
+    });
+
     const app = __dirname + '/../../assets/home/index.html';
     fs.createReadStream(app).pipe(res)
   }
