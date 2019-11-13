a32e833f75e52791052c221a6872112c30c465c7
gumball <ugis.rozkalns@gmail.com>
2019-11-13 10:12:36 +0800

    Queue cleanup for DEV/Staging

--- api/hooks/jobs/emailQueue.js
+++ api/hooks/jobs/emailQueue.js
@@ -1,87 +1,92 @@
-var Queue = require('bull');
-var paymentEmailQueue = new Queue('PaymentEmailQueue', sails.config.jobs.url);
-var emailTriggerQueue = new Queue('EmailTriggerQueue', sails.config.jobs.url);
+if (process.env.NODE_ENV !== 'production' || process.env.sails_environment === 'staging') {
+  return false
+} else {
+  var Queue = require('bull');
 
-emailTriggerQueue.process('SendEmails',100,async function (job){
-  // Send Failed Payment Email
+  var paymentEmailQueue = new Queue('PaymentEmailQueue', sails.config.jobs.url);
+  var emailTriggerQueue = new Queue('EmailTriggerQueue', sails.config.jobs.url);
 
-  let failedPayments = await TransactionsLog.find({
-    where: {
-      type: 'S_ERROR',
-      serialize: {
-        '!=': 's:49:"Error Occured while retrieving Stripe Customer ID";'
-      },
-      createdAt: {
-        '>=': new Date(Date.now() - 4 * 60 * 60 * 1000 - 15 * 60 * 1000),
-        '<': new Date(Date.now() - 4 * 60 * 60 * 1000),
-      }
-    }
-  });
+  emailTriggerQueue.process('SendEmails', 100, async function (job) {
+    // Send Failed Payment Email
 
-  failedPayments.forEach(function (payment) {
-    if (payment.user_id && payment.user_id !== 0) {
-      paymentEmailQueue.add('SendEmail', {
-          payment
+    let failedPayments = await TransactionsLog.find({
+      where: {
+        type: 'S_ERROR',
+        serialize: {
+          '!=': 's:49:"Error Occured while retrieving Stripe Customer ID";'
         },
-        {
-          attempts: 2,
-          timeout: 120000
-        })
-    }
+        createdAt: {
+          '>=': new Date(Date.now() - 4 * 60 * 60 * 1000 - 15 * 60 * 1000),
+          '<': new Date(Date.now() - 4 * 60 * 60 * 1000),
+        }
+      }
+    });
+
+    failedPayments.forEach(function (payment) {
+      if (payment.user_id && payment.user_id !== 0) {
+        paymentEmailQueue.add('SendEmail', {
+            payment
+          },
+          {
+            attempts: 2,
+            timeout: 120000
+          })
+      }
+    });
   });
-});
 
-emailTriggerQueue.removeRepeatable('SendEmails',{repeat: {cron: '*/15 * * * *'}});
+  emailTriggerQueue.removeRepeatable('SendEmails', {repeat: {cron: '*/15 * * * *'}});
 
-paymentEmailQueue.process('SendEmail', 100,async function (job, done){
+  paymentEmailQueue.process('SendEmail', 100, async function (job, done) {
 
-  sails.log.info({job: job.data});
+    sails.log.info({job: job.data});
 
-  let userData = await User.findOne({id: job.data.payment.user_id});
+    let userData = await User.findOne({id: job.data.payment.user_id});
 
-  let description = '';
-  try {
-    description = job.data.payment.response.split('"')[1];
-  } catch (e) {
-    sails.hooks.bugsnag.notify(e)
-  }
+    let description = '';
+    try {
+      description = job.data.payment.response.split('"')[1];
+    } catch (e) {
+      sails.hooks.bugsnag.notify(e)
+    }
 
-  let message = `ChinesePod Team,\n\nA user payment has failed. Additional information below:\nUser: ${userData.name}\nCRM: https://www2.chinesepod.com/marketingcenter/users/index/view?user_id=${userData.id}\nEmail: ${userData.email}\nDescription: ${description}\n\nSincerely,\nThe Reporting System`;
+    let message = `ChinesePod Team,\n\nA user payment has failed. Additional information below:\nUser: ${userData.name}\nCRM: https://www2.chinesepod.com/marketingcenter/users/index/view?user_id=${userData.id}\nEmail: ${userData.email}\nDescription: ${description}\n\nSincerely,\nThe Reporting System`;
 
-  let Mailgun = require('machinepack-mailgun');
+    let Mailgun = require('machinepack-mailgun');
 
-  Mailgun.sendPlaintextEmail({
+    Mailgun.sendPlaintextEmail({
 
-    apiKey: sails.config.custom.mailgunSecret,
+      apiKey: sails.config.custom.mailgunSecret,
 
-    domain: sails.config.custom.mailgunDomain,
+      domain: sails.config.custom.mailgunDomain,
 
-    toEmail: 'followup@chinesepod.com',
+      toEmail: 'followup@chinesepod.com',
 
-    toName: 'ChinesePod Followup Team',
+      toName: 'ChinesePod Followup Team',
 
-    subject: 'Stripe New Subscription Payment Error!',
+      subject: 'Stripe New Subscription Payment Error!',
 
-    message: message,
+      message: message,
 
-    fromEmail: 'stripe@chinesepod.com',
+      fromEmail: 'stripe@chinesepod.com',
 
-    fromName: 'ChinesePod Stripe',
+      fromName: 'ChinesePod Stripe',
 
-  }).exec({
+    }).exec({
 
 // An unexpected error occurred.
 
-    error: function (err) {
-      sails.hooks.bugsnag.notify(err);
-    },
+      error: function (err) {
+        sails.hooks.bugsnag.notify(err);
+      },
 
 // OK.
 
-    success: function () {
+      success: function () {
 
-    },
+      },
+    });
+
+    done();
   });
-
-  done();
-});
+}
--- api/hooks/jobs/loggingQueue.js
+++ api/hooks/jobs/loggingQueue.js
@@ -1,57 +1,62 @@
-var Queue = require('bull');
+if (process.env.NODE_ENV !== 'production' || process.env.sails_environment === 'staging') {
+  return false
+} else {
+  var Queue = require('bull');
 
-var loggingQueue = new Queue('LoggingQueue', sails.config.jobs.url);
+  var loggingQueue = new Queue('LoggingQueue', sails.config.jobs.url);
 
-loggingQueue.on('ready', () => {
-  sails.log.info('loggingQueue ready!');
-});
-loggingQueue.on('failed', (job, e) => {
-  sails.log.error('loggingQueue failed:', job.id, e);
-});
+  loggingQueue.on('ready', () => {
+    sails.log.info('loggingQueue ready!');
+  });
+  loggingQueue.on('failed', (job, e) => {
+    sails.log.error('loggingQueue failed:', job.id, e);
+  });
 
-loggingQueue.process('Logging Requests', 100,async function (job, done) {
-  if (!job.data) {
-    done( null, 'No job data')
-  }
-  let userData = {};
+  loggingQueue.process('Logging Requests', 100,async function (job, done) {
+    if (!job.data) {
+      done( null, 'No job data')
+    }
+    let userData = {};
 
-  if (job.data.userId) {
-    userData = await User.findOne({id: job.data.userId});
-  }
+    if (job.data.userId) {
+      userData = await User.findOne({id: job.data.userId});
+    }
 
-  let ipData = {};
+    let ipData = {};
 
-  if(job.data.ip && job.data.ip !== '::1' && [
-    'https://www.chinesepod.com/dash',
-    'https://www.chinesepod.com/home',
-    'https://www.chinesepod.com/signup',
-    'https://www.chinesepod.com/checkout'].includes(job.data.urlbase)) {
-    const ipdata =  require('ipdata');
-    await ipdata.lookup(job.data.ip, sails.config.custom.ipDataKey)
-      .then((info) => {ipData = info})
-      .catch((err) => sails.log.error(err));
-  }
+    if(job.data.ip && job.data.ip !== '::1' && [
+      'https://www.chinesepod.com/dash',
+      'https://www.chinesepod.com/home',
+      'https://www.chinesepod.com/signup',
+      'https://www.chinesepod.com/checkout'].includes(job.data.urlbase)) {
+      const ipdata =  require('ipdata');
+      await ipdata.lookup(job.data.ip, sails.config.custom.ipDataKey)
+        .then((info) => {ipData = info})
+        .catch((err) => sails.log.error(err));
+    }
 
-  if ([
-    'https://www.chinesepod.com/dash',
-    'https://www.chinesepod.com/home',
-    'https://www.chinesepod.com/signup',
-    'https://www.chinesepod.com/checkout',
-    'https://www.chinesepod.com/login'
-  ].includes(job.data.urlbase) || (userData && userData.email)) {
-    await Logging.create({
-      id: userData.email ? userData.email : 'NONE',
-      access_ip: job.data.ip,
-      accesslog_url: job.data.url,
-      accesslog_sessionid: job.data.sessionId,
-      accesslog_urlbase: job.data.urlbase,
-      accesslog_country: ipData['country_name'] ? ipData['country_name'] : null,
-      referer: job.data.referer
-    });
-    done(null, userData.email ? `Logged Request for User: ${userData.email}` : `Logged Request for Unknown User`)
-  } else {
-    done(null, 'Ignoring this log')
-  }
-});
+    if ([
+      'https://www.chinesepod.com/dash',
+      'https://www.chinesepod.com/home',
+      'https://www.chinesepod.com/signup',
+      'https://www.chinesepod.com/checkout',
+      'https://www.chinesepod.com/login'
+    ].includes(job.data.urlbase) || (userData && userData.email)) {
+      await Logging.create({
+        id: userData.email ? userData.email : 'NONE',
+        access_ip: job.data.ip,
+        accesslog_url: job.data.url,
+        accesslog_sessionid: job.data.sessionId,
+        accesslog_urlbase: job.data.urlbase,
+        accesslog_country: ipData['country_name'] ? ipData['country_name'] : null,
+        referer: job.data.referer
+      });
+      done(null, userData.email ? `Logged Request for User: ${userData.email}` : `Logged Request for Unknown User`)
+    } else {
+      done(null, 'Ignoring this log')
+    }
+  });
 
-module.exports = loggingQueue;
+  module.exports = loggingQueue;
+}
+
