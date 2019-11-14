eaf6d894c135f5c664ff23a8fdc29f112d465966
gumball <ugis.rozkalns@gmail.com>
2019-11-14 15:35:43 +0800

    Adding Main Android App API routes

---
+++ api/controllers/ads/android-app.js
@@ -0,0 +1,51 @@
+module.exports = {
+
+
+  friendlyName: 'Android app',
+
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: function (inputs) {
+
+
+    const adObject = `<VAST xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="vast.xsd" version="3.0">
+    <Ad id="6972004961">
+        <InLine>
+            <AdSystem>GDFP</AdSystem>
+            <AdTitle/>
+            <Description>
+            </Description>
+            <Creatives>
+                <Creative id="578604590561" sequence="1">
+                    <Linear skipoffset="00:00:05">
+                        <Duration>00:03:06</Duration>
+                        <MediaFiles>
+                            <MediaFile id="GDFP" delivery="progressive" width="1280" height="720" type="video/mp4" bitrate="533" scalable="true" maintainAspectRatio="true">
+                                <![CDATA[https://embed.wistia.com/deliveries/ad2c327bb68922ccec498c30db33cc6c302135f4.bin/videoAD.mp4]]>
+                            </MediaFile>
+                        </MediaFiles>
+                    </Linear>
+                </Creative>
+            </Creatives>
+        </InLine>
+    </Ad>
+</VAST>`;
+
+    this.res.set({'Content-Type': 'application/xml'}).send(adObject)
+
+  }
+
+
+};
---
+++ api/controllers/tutorials/android-intro-video.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+
+  friendlyName: 'Android intro video',
+
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // All done.
+    return {
+      success: 1,
+      video: '2i4c15ciif'
+    }
+
+  }
+
+
+};
