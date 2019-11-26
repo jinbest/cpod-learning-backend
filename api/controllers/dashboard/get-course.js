544af204347e907fddef36f1b8e9b5a0362c1036
gumball <ugis.rozkalns@gmail.com>
2019-11-26 16:36:41 +0800

    v0.3.20

---
+++ api/controllers/dashboard/get-course.js
@@ -0,0 +1,67 @@
+module.exports = {
+
+
+  friendlyName: 'Course lessons',
+
+
+  description: '',
+
+
+  inputs: {
+    courseId: {
+      type: 'number',
+      isInteger: true,
+      required: true
+    },
+    limit: {
+      type: 'number',
+      isInteger: true
+    },
+    studied: {
+      type: 'boolean'
+    },
+    exclude: {
+      type: ['ref'],
+      description: 'list of lessonIDs to exclude'
+    }
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    inputs.userId = sails.config.environment === 'development' ? 1016995 : this.req.session.userId;
+
+    let courseData = await CourseContents.find({
+      where: {course_id: inputs.courseId},
+      select: ['course_id', 'lesson', 'displaysort'],
+      limit: 50 // inputs.limit ? inputs.limit : 50
+    }).populate('lesson.userContents', {
+      where: {
+        user_id: inputs.userId,
+        lesson_type: 0
+      },
+      select: ['saved', 'studied']
+    });
+
+    let returnData = [];
+    courseData.forEach((lesson) => {
+      if (lesson.lesson.userContents[0]) {
+        lesson.lesson.saved = lesson.lesson.userContents[0].saved;
+        lesson.lesson.studied = lesson.lesson.userContents[0].studied;
+        delete lesson.lesson.userContents;
+        returnData.push(lesson.lesson);
+      } else {
+        returnData.push(lesson.lesson);
+      }
+    });
+
+    return (returnData.filter((lesson) => {return lesson.studied !== 1 && (inputs.exclude ? !inputs.exclude.includes(lesson.id) : true)})).slice(0, inputs.limit)
+  }
+
+
+};
