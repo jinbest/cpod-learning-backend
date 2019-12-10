8aad1cfbef85b6550efbb08300376b1a3ff5ec86
gumball <ugis.rozkalns@gmail.com>
2019-12-10 13:41:01 +0800

    Feedback Emails

--- api/controllers/feedback/dashboard-feedback.js
+++ api/controllers/feedback/dashboard-feedback.js
@@ -65,7 +65,37 @@ module.exports = {
     }
 
     try {
+
+      await sails.helpers.mailgun.sendHtmlEmail.with({
+        htmlMessage: `
+            ${inputs.wantsResponse ? '<p>USER WANTS TO HEAR BACK</p>' : ''}
+            ${inputs.wantsToParticipate ? '<p>USER WANTS TO BETA TEST</p>' : ''}
+            <br />
+            <p>User has submitted feedback on the New ChinesePod Dashboard</p>
+            <br />
+            <p>Name: ${inputs.name}</p>
+            <p>Email: ${inputs.email}</p>
+            <br />
+            <p>Feedback: ${inputs.feedback}</p>
+            <p>Likely to Switch: ${inputs.likelyToSwitch}</p>            
+            <br />
+            <p>Comments: ${inputs.comments}</p>            
+            <br />
+            <p>How User Studies: ${inputs.howToStudy}</p>           
+            <br />
+            <p>Primary Device: ${inputs.primaryDevice === 'other' ? inputs.primaryDevice : inputs.other}</p>
+            <br />
+            <p>Cheers,</p>
+            <p>The Friendly ChinesePod Contact Robot</p>
+            `,
+        to: 'feedback@chinesepod.com',
+        subject: 'ChinesePod Dashboard Feedback',
+        from: 'feedback@chinesepod.com',
+        fromName: 'ChinesePod Feedback'
+      });
+
       await FeedbackForms.create(inputs);
+
     } catch (e) {
       sails.log.error({inputs: inputs});
       sails.hooks.bugsnag.notify(e)
