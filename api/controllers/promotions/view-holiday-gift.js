208189c5e1bcdb9cc2e964f8dc2661f555d7f66a
gumball <ugis.rozkalns@gmail.com>
2019-12-18 10:42:37 +0800

    Holiday gift redirect

--- api/controllers/promotions/view-holiday-gift.js
+++ api/controllers/promotions/view-holiday-gift.js
@@ -19,6 +19,17 @@ module.exports = {
   fn: async function () {
 
     // Respond with view.
+    const geoip = require('geoip-country');
+
+    try {
+      let geo = geoip.lookup(this.req.ip);
+      if (geo['country'] === 'US') {
+        return this.res.redirect('/')
+      }
+    } catch (e) {
+      sails.log.error(e)
+    }
+
     return {};
 
   }
