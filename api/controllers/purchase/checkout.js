eae2ed127cd4d968eca613bdb358bbdc66ee80f6
gumball <ugis.rozkalns@gmail.com>
2019-12-20 15:55:22 +0800

    Analytics Item name fix

--- api/controllers/purchase/checkout.js
+++ api/controllers/purchase/checkout.js
@@ -644,7 +644,7 @@ module.exports = {
 
         this.req.visitor
           .event('payment', 'payment')
-          .transaction(transaction.id, transaction.billed_amount)
+          // .transaction(transaction.id, transaction.billed_amount)
           .item(transaction.billed_amount, 1, transaction.product_id, productName, {ti:  transaction.id})
           .send();
 
