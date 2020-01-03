b282db109f8b3fb228f9c3cf229f343def338418
gumball <ugis.rozkalns@gmail.com>
2020-01-03 16:36:18 +0800

    Revising Shipping package logic

--- api/controllers/labels/gift-package/get-gift-package-labels.js
+++ api/controllers/labels/gift-package/get-gift-package-labels.js
@@ -32,16 +32,33 @@ module.exports = {
 
     let sentPackages = (await GiftTracking.find().select(['id'])).map((item) => {return item.id});
 
+    let oldTransactions = require('../../../../lib/oldTransactions.json');
+
+    // let oldTransactions = (await sails.sendNativeQuery(`
+    //   SELECT distinct t.user_id FROM chinesepod_production.transactions t WHERE t.pay_status=2 AND t.date_created < '2019-12-20'
+    // `))['rows'];
+    //
+    // const fs = require('fs');
+    //
+    // oldTransactions = oldTransactions.map(transaction => {return transaction.user_id});
+    //
+    // fs.writeFileSync('oldTransactions.json', JSON.stringify(oldTransactions));
+
     let relevantTransactions = await Transactions.find({
       where: {
         user_id: {
-          nin: sentPackages
+          nin: sentPackages.concat(oldTransactions)
         },
+        pay_status: 2,
         is_recurring_payment: 0,
         createdAt: {
           '>=': startDate
         }
       }
+    });
+
+    relevantTransactions = _.uniq(relevantTransactions, function (item) {
+      return item.user_id
     });
 
     relevantTransactions = relevantTransactions.map(transaction => {return transaction.id});
