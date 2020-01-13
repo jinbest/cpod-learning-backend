431139e01176db58d70e755c03fee1553d005f06
gumball <ugis.rozkalns@gmail.com>
2020-01-13 16:03:49 +0800

    Gift package api changes

--- api/controllers/labels/gift-package/get-gift-package-labels.js
+++ api/controllers/labels/gift-package/get-gift-package-labels.js
@@ -34,14 +34,13 @@ module.exports = {
 
     let oldTransactions = require('../../../../lib/oldTransactions.json');
 
-    // let oldTransactions = (await sails.sendNativeQuery(`
-    //   SELECT distinct t.user_id FROM chinesepod_production.transactions t WHERE t.pay_status=2 AND t.date_created < '2019-12-20'
-    // `))['rows'];
-    //
+    // ', '', ' let oldTransactions = (await sails.sendNativeQuery(`
+    // ', '', '   SELECT distinct t.user_id FROM chinesepod_production.transactions t WHERE t.pay_status=2 AND t.date_created < '2019-12-20'
+    // ', '', ' `))['rows'];
+
     // const fs = require('fs');
     //
     // oldTransactions = oldTransactions.map(transaction => {return transaction.user_id});
-    //
     // fs.writeFileSync('oldTransactions.json', JSON.stringify(oldTransactions));
 
     let relevantTransactions = await Transactions.find({
@@ -75,6 +74,25 @@ module.exports = {
       },
       select: ['transaction_id', 'country', 'state', 'city', 'zip_code', 'full_name', 'address1', 'address2'],
       sort: 'updatedAt DESC'
+    });
+
+    const europeanAddresses = ['AT', 'BE', 'CH', 'DE', 'DK', 'FR', 'FI', 'NL', 'PT', 'RU', 'SE', 'NO', 'UA', 'UK'];
+
+    const asiaAddresses = ['HK', 'CN', 'TW'];
+
+    addresses.forEach(address => {
+      let ship_from = `ChinesePod LLC | 246 West Broadway | New York NY 10013`;
+      if (europeanAddresses.includes(address.country.toUpperCase())) {
+        ship_from = `ChinesePod | P.O. Box 92002 | NL-1090 AA Amsterdam`;
+      } else if (asiaAddresses.includes(address.country.toUpperCase())) {
+        ship_from = `ChinesePod Limited | General Post Office Box 7347 | Hong Kong S.A.R.`;
+      } else if ('PH' === address.country.toUpperCase()) {
+        ship_from = `Lapu-Lapu City Post Office Box 16 | 6015 Lapu-Lapu City | Philippines`;
+      }
+
+      address.ship_from = ship_from;
+      address.ship_to = `${address.address1.length > 0 ? address.address1 + '\n' : ''}${address.address2.length > 0 ? address.address2 + '\n' : ''}${address.city ? address.city : ''} ${address.state ? address.state : ''} ${address.zip_code ? address.zip_code : ''}`;
+
     });
 
     return {
