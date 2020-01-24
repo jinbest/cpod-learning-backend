4b8bfa1e44018ebcbc80c57ef3d08756a7628965
gumball <ugis.rozkalns@gmail.com>
2020-01-24 11:25:15 +0800

    Alice Email Queue - Inactivity

--- api/hooks/jobs/emailQueue.js
+++ api/hooks/jobs/emailQueue.js
@@ -142,7 +142,7 @@ if (process.env.NODE_ENV !== 'production' || process.env.sails_environment === '
 
   emailTriggerQueue.process('ScheduleInactivityEmails', 1, async function (job, done) {
 
-    let users = ['ugis@chinesepod', 'planetugis@gmail.com', 'ugis.rozkalns@gmail.com'];
+    let users = ['ugis@chinesepod.com', 'planetugis@gmail.com', 'ugis.rozkalns@gmail.com'];
 
     let userData = await sails.models['user'].find({email: {in: users}});
 
