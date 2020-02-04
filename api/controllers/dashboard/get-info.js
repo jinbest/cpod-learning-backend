613ddafb4dcf7af41fcc73c5117fd05fdc285d1a
gumball <ugis.rozkalns@gmail.com>
2020-02-04 18:32:31 +0800

    Info fix

--- api/controllers/dashboard/get-info.js
+++ api/controllers/dashboard/get-info.js
@@ -53,7 +53,7 @@ module.exports = {
       returnData.charSetUnset = true;
     }
 
-    let userPreferences = await UserPreferences.findOne(inputs.userId);
+    let userPreferences = (await UserPreferences.find(inputs.userId).limit(1))[0];
 
     let access = await sails.helpers.users.getAccessType(inputs.userId);
 
