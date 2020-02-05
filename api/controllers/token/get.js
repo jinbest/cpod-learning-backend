c4cd68e94a4b5e005753aee00e40eaf5c385c9a0
gumball <ugis.rozkalns@gmail.com>
2020-02-05 18:49:25 +0800

    Search work

--- api/controllers/token/get.js
+++ api/controllers/token/get.js
@@ -28,10 +28,10 @@ module.exports = {
 
 
   fn: async function (inputs) {
-    inputs.userId = sails.config.environment === 'development' ? 1016995 : this.req.session.userId;
+    inputs.userId = sails.config.environment === 'development' ? 1016995 : this.req.session.me.id;
 
     const validApps = {
-      dashboard: ['0r6jo0purpo86683joyeq6tpw8n61tyzcw53yrw9'],
+      dashboard: ['0r6jo0purpo86683joyeq6tpw8n61tyzcw53yrw9', '7X9w@A!AQJ8$4$4#2eNszv*ecY9s09n3c!Py$b8I'], // 7X9w... added on Feb 05
       recap: ['fsl5rctm7rmw4s1byz4hqocwwz2t04b3u36b4dxt'],
       testing: ['aaaa1111bbbb2222cccc3333'],
     };
