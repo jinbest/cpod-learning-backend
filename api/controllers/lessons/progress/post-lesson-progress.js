9511e16fd3797c651f8069713f87aa11bd89f166
gumball <ugis.rozkalns@gmail.com>
2020-02-07 16:38:20 +0800

    Progress bugfix

--- api/controllers/lessons/progress/post-lesson-progress.js
+++ api/controllers/lessons/progress/post-lesson-progress.js
@@ -29,15 +29,25 @@ module.exports = {
   fn: async function (inputs) {
     inputs.userId = sails.config.environment === 'development' ? 1016995 : this.req.session.userId;
 
-    let currentProgress = await LessonProgress.findOne({userId: inputs.userId, lessonId: inputs.lessonId, track_type: inputs.type});
+    sails.log.info(inputs);
+
+    let progress = await LessonProgress.find({userId: inputs.userId, lessonId: inputs.lessonId, track_type: inputs.track});
+
+    let currentProgress = progress[0];
+
+    if (progress.length > 1) {
+
+      await LessonProgress.destroy({id: progress.slice(1).map(item => item.id)});
+
+    }
 
     if (!currentProgress) {
 
-      return await LessonProgress.create({userId: inputs.userId, lessonId: inputs.lessonId, track_type: inputs.type, progress: inputs.progress}).fetch()
+      return await LessonProgress.create({userId: inputs.userId, lessonId: inputs.lessonId, track_type: inputs.track, progress: inputs.progress / 100})
 
     } else if (inputs.progress > currentProgress.progress * 100){
 
-      return await LessonProgress.updateOne({id: currentProgress.id}).set({userId: inputs.userId, lessonId: inputs.lessonId, track_type: inputs.type, progress: inputs.progress / 100})
+      return await LessonProgress.updateOne({id: currentProgress.id}).set({userId: inputs.userId, lessonId: inputs.lessonId, track_type: inputs.track, progress: inputs.progress / 100})
 
     }
 
