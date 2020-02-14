84d97362fb1f427a139cc2b429c169bfef01f13f
gumball <ugis.rozkalns@gmail.com>
2020-02-14 08:38:40 +0800

    Dashboard Update

--- assets/home/service-worker.js
+++ assets/home/service-worker.js
@@ -15,7 +15,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 
 importScripts(
   "/home/updateSW.js",
-  "/home/precache-manifest.890ac5de96f36bda6d978d38159adb6c.js"
+  "/home/precache-manifest.0d0ded6fc31afe8d0c639d6f7ba82c2d.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "chinesepod-dashboard"});
