8f5f76a3070ae0230949a28476a56e2d5ce6a956
gumball <ugis.rozkalns@gmail.com>
2020-03-04 12:21:02 +0800

    Disabling Mautic

--- api/helpers/mautic/create-contact.js
+++ api/helpers/mautic/create-contact.js
@@ -41,32 +41,34 @@ module.exports = {
 
   fn: async function (inputs) {
 
-    //MAUTIC
-    const MauticConnector = require('node-mautic');
-    const mauticConnector = new MauticConnector({
-      apiUrl: 'https://email.chinesepod.com',
-      username: 'CpodJsWebsite',
-      password: 'zro5YdSykdqYkkgPMBH9yCcGPguGdAbk8IXyjnCW'
-    });
-
-    const newContact = await mauticConnector.contacts.createContact({
-      email: inputs.email,
-      firstname: inputs.firstName ? inputs.firstName : '',
-      lastname: inputs.lastName ? inputs.lastName : '',
-      fullname: inputs.fullName ? inputs.fullName : '',
-      userid: inputs.userId ? inputs.userId : '',
-      promo: inputs.optIn,
-      academic: inputs.optIn,
-      activity: inputs.optIn,
-      shows: inputs.optIn,
-      newsletter: inputs.optIn,
-      meetup: inputs.optIn,
-      country: inputs.ipData['country_name'],
-      region: inputs.ipData['region'],
-      city: inputs.ipData['city'],
-    });
-
-    return newContact
+    return inputs;
+    //
+    // //MAUTIC
+    // const MauticConnector = require('node-mautic');
+    // const mauticConnector = new MauticConnector({
+    //   apiUrl: 'https://email.chinesepod.com',
+    //   username: 'CpodJsWebsite',
+    //   password: 'zro5YdSykdqYkkgPMBH9yCcGPguGdAbk8IXyjnCW'
+    // });
+    //
+    // const newContact = await mauticConnector.contacts.createContact({
+    //   email: inputs.email,
+    //   firstname: inputs.firstName ? inputs.firstName : '',
+    //   lastname: inputs.lastName ? inputs.lastName : '',
+    //   fullname: inputs.fullName ? inputs.fullName : '',
+    //   userid: inputs.userId ? inputs.userId : '',
+    //   promo: inputs.optIn,
+    //   academic: inputs.optIn,
+    //   activity: inputs.optIn,
+    //   shows: inputs.optIn,
+    //   newsletter: inputs.optIn,
+    //   meetup: inputs.optIn,
+    //   country: inputs.ipData['country_name'],
+    //   region: inputs.ipData['region'],
+    //   city: inputs.ipData['city'],
+    // });
+    //
+    // return newContact
 
   }
 
