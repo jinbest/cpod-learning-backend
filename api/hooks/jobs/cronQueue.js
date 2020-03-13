44631e50b40becf954788c280b3c9d5757ea227e
gumball <ugis.rozkalns@gmail.com>
2020-03-13 17:08:09 +0800

    Cleanup

--- api/hooks/jobs/cronQueue.js
+++ api/hooks/jobs/cronQueue.js
@@ -6,20 +6,19 @@ if (process.env.NODE_ENV !== 'production' || sails.config.environment === 'stagi
   var triggerQueue = new Queue('TriggerQueue', sails.config.jobs.url);
   triggerQueue.clean(0);
 
-  triggerQueue.process('ReindexLessons', 1, async function (job) {
+  triggerQueue.process('ReindexLessons', 1, async function () {
     await sails.helpers.search.reindexLessons();
     await sails.helpers.search.reindexCourses();
   });
   triggerQueue.add('ReindexLessons', {data: 'Reindex Lessons Once a Day'}, {repeat: {cron: '0 0 * * *'}});
 
-  triggerQueue.process('ReindexSentences', 1, async function (job) {
+  triggerQueue.process('ReindexSentences', 1, async function () {
     await sails.helpers.search.reindexSentences();
   });
   triggerQueue.add('ReindexSentences', {data: 'Reindex Lessons Once a Day'}, {repeat: {cron: '0 1 * * *'}});
 
-  triggerQueue.process('NewLessonNotifications', 1, async function (job) {
+  triggerQueue.process('NewLessonNotifications', 1, async function () {
     await sails.helpers.notifications.newLesson();
   });
   triggerQueue.add('NewLessonNotifications', {data: 'Check Lessons Every 15 min'}, {repeat: {every: 15 * 60 * 1000}});
-
 }
