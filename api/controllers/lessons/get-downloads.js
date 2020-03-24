0d3adce34f1f768f688e150cbb8e80fcef0431c6
gumball <ugis.rozkalns@gmail.com>
2020-03-24 17:46:08 +0800

    Downloads fix

--- api/controllers/lessons/get-downloads.js
+++ api/controllers/lessons/get-downloads.js
@@ -27,13 +27,17 @@ module.exports = {
     inputs.userId = sails.config.environment === 'development' ? 1016995 : this.req.session.userId;
 
     let access = await sails.helpers.users.getAccessType(inputs.userId);
+
+    let lessonData = await LessonData.findOne({id: inputs.lessonId});
+
+    let extra = lessonData.type === 'extra';
+
+    let returnData = {
+      type: access,
+      downloads: {}
+    };
+
     if (access === 'premium' || access === 'admin') {
-      let lessonData = await LessonData.findOne({id: inputs.lessonId});
-      let returnData = {
-        type: 'premium',
-        downloads: {}
-      };
-      let extra = lessonData.type === 'extra';
       if (lessonData.mp3_private) {
         returnData.downloads.lesson = '/' + (extra ? 'extra/' : '') + lessonData.id + '/' + lessonData.hash_code + '/' + lessonData.mp3_private;
       }
@@ -49,23 +53,15 @@ module.exports = {
       if (lessonData.pdf2) {
         returnData.downloads.pdf2 = '/' + (extra ? 'extra/' : '') + lessonData.id + '/' + lessonData.hash_code + '/' + lessonData.pdf2;
       }
-      return returnData;
     } else if (access === 'basic') {
-      let lessonData = await LessonData.findOne({id: inputs.lessonId});
-      let returnData = {
-        type: 'basic',
-        downloads: {}
-      };
       if (lessonData.pdf1) {
         returnData.downloads.pdf1 = '/' + (extra ? 'extra/' : '') + lessonData.id + '/' + lessonData.hash_code + '/' + lessonData.pdf1;
       }
       if (lessonData.pdf2) {
         returnData.downloads.pdf2 = '/' + (extra ? 'extra/' : '') + lessonData.id + '/' + lessonData.hash_code + '/' + lessonData.pdf2;
       }
-    }else {
-      return {
-        type: 'free'
-      }
     }
+
+    return returnData;
   }
 };
--- config/custom.js
+++ config/custom.js
@@ -116,7 +116,7 @@ module.exports.custom = {
 
   // ADVERTISING PROMOS
   coreMarkets: ['US', 'CA', 'CN'],
-  coreFreeMonths: [0,2,4,6,8,10],
-  nonCoreFreeMonths: [1,3,5,7,9,11],
+  coreFreeMonths: [0,2,3,4,6,8,10],
+  nonCoreFreeMonths: [1,2,3,5,7,9,11],
   overrideWindow: false  // TODO add a time window constraint as needed
 };
