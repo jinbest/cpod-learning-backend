85d59a620997e5f08150b6826db6e24e7367ecef
gumball <ugis.rozkalns@gmail.com>
2020-04-08 18:39:11 +0800

    Cleanup

--- api/helpers/search/index-lesson.js
+++ api/helpers/search/index-lesson.js
@@ -24,73 +24,56 @@ module.exports = {
   fn: async function (inputs) {
 
     let index = {
-        model: 'LessonData',
-        elasticModel: 'LessonDataIndex',
-        elasticIndex: 'lessons',
-        elasticRecord: [
-          'id',
-          'status_published',
-          'slug',
-          'title',
-          'introduction',
-          'type',
-          'publication_timestamp',
-          'level',
-          'levelId',
-          'hosts',
-          'hash_code',
-          'image',
-          'transcription1'
-        ],
-        idColumn: 'id'
-      };
+      model: 'LessonData',
+      elasticModel: 'LessonDataIndex',
+      elasticIndex: 'lessons',
+      elasticRecord: [
+        'id',
+        'status_published',
+        'slug',
+        'title',
+        'introduction',
+        'type',
+        'publication_timestamp',
+        'level',
+        'levelId',
+        'hosts',
+        'hash_code',
+        'image',
+        'transcription1'
+      ],
+      idColumn: 'id'
+    };
 
-    await new Promise(async (resolve, reject) => {
-
-      let records = await LessonData.find({
-        where: {
-          id: inputs.lessonId,
-          status_published: 'publish',
-          is_private: 0
-        },
-        sort: 'publication_timestamp DESC'
-      });
-
-
-      const levelIds = {
-        newbie: 1,
-        elementary: 2,
-        'pre intermediate': 3,
-        intermediate: 4,
-        'upper intermediate': 5,
-        advanced: 6,
-        media: 7,
-        any: 0
-      };
-
-      records.forEach(record => {
-        let indexRecord = {};
-        index.elasticRecord.forEach(key => {
-          indexRecord[key] = record[key];
-        });
-
-        indexRecord['levelId'] = levelIds[indexRecord['level'].toLowerCase()];
-
-        commands.push(action);
-        commands.push(indexRecord);
-      });
-
-      sails.log.info('Records Prepared');
-
-      // run bulk command
-      await sails.hooks.elastic.client.index({index: index.elasticIndex, body: indexRecord}, (error, response) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(response);
-        }
-      });
-
+    let lessonData = await LessonData.findOne({
+      id: inputs.lessonId,
+      status_published: 'publish',
+      is_private: 0
     });
+
+
+    const levelIds = {
+      newbie: 1,
+      elementary: 2,
+      'pre intermediate': 3,
+      intermediate: 4,
+      'upper intermediate': 5,
+      advanced: 6,
+      media: 7,
+      any: 0
+    };
+
+
+    let indexRecord = {};
+    index.elasticRecord.forEach(key => {
+      indexRecord[key] = lessonData[key];
+    });
+
+    indexRecord['levelId'] = levelIds[indexRecord['level'].toLowerCase()];
+
+    await sails.hooks.elastic.client.index({index: index.elasticIndex, id: indexRecord['id'], body: indexRecord})
+      .catch(error => sails.hooks.bugsnag.notify(error));
+    await sails.hooks.elastic.client.indices.refresh({index: index.elasticIndex})
+      .catch(error => sails.hooks.bugsnag.notify(error))
   }
 };
--- api/helpers/search/reindex-lessons.js
+++ api/helpers/search/reindex-lessons.js
@@ -20,96 +20,81 @@ module.exports = {
   fn: async function (inputs) {
 
     let index = {
-        model: 'LessonData',
-        elasticModel: 'LessonDataIndex',
-        elasticIndex: 'lessons',
-        elasticRecord: [
-          'id',
-          'status_published',
-          'slug',
-          'title',
-          'introduction',
-          'type',
-          'publication_timestamp',
-          'level',
-          'levelId',
-          'hosts',
-          'hash_code',
-          'image',
-          'transcription1'
-        ],
-        idColumn: 'id'
-      };
+      model: 'LessonData',
+      elasticModel: 'LessonDataIndex',
+      elasticIndex: 'lessons',
+      elasticRecord: [
+        'id',
+        'status_published',
+        'slug',
+        'title',
+        'introduction',
+        'type',
+        'publication_timestamp',
+        'level',
+        'levelId',
+        'hosts',
+        'hash_code',
+        'image',
+        'transcription1'
+      ],
+      idColumn: 'id'
+    };
 
-    await new Promise(async (resolve, reject) => {
+    await sails.hooks.elastic.client.indices.delete({index: index.elasticIndex})
+      .catch(error => {});
 
-      await sails.hooks.elastic.client.indices.delete({index: index.elasticIndex}, (error, response) => {
-        if (error) {
-          sails.log.error(error);
+    sails.log.info('Index Deleted');
+
+    let records = await LessonData.find({
+      where: {
+        status_published: 'publish',
+        is_private: 0,
+        publication_timestamp: {
+          '<': new Date()
         }
+      },
+      sort: 'publication_timestamp DESC'
+    });
+
+    sails.log.info('Records Pooled');
+
+    let commands = [];
+
+    const levelIds = {
+      newbie: 1,
+      elementary: 2,
+      'pre intermediate': 3,
+      intermediate: 4,
+      'upper intermediate': 5,
+      advanced: 6,
+      media: 7,
+      any: 0
+    };
+
+
+    records.forEach(record => {
+      let indexRecord = {};
+      index.elasticRecord.forEach(key => {
+        indexRecord[key] = record[key];
       });
 
-      sails.log.info('Index Deleted');
+      indexRecord['levelId'] = levelIds[indexRecord['level'].toLowerCase()];
 
-
-      let records = await LessonData.find({
-        where: {
-          status_published: 'publish',
-          is_private: 0,
-          publication_timestamp: {
-            '<': new Date()
-          }
-        },
-        sort: 'publication_timestamp DESC'
-      });
-
-      sails.log.info('Records Pooled');
-
-      let commands = [];
-      let action = {
+      commands.push({
         index: {
           _index: index.elasticIndex,
-          _type: index.elasticIndex
-        }
-      };
-
-
-      const levelIds = {
-        newbie: 1,
-        elementary: 2,
-        'pre intermediate': 3,
-        intermediate: 4,
-        'upper intermediate': 5,
-        advanced: 6,
-        media: 7,
-        any: 0
-      };
-
-
-      records.forEach(record => {
-        let indexRecord = {};
-        index.elasticRecord.forEach(key => {
-          indexRecord[key] = record[key];
-        });
-
-        indexRecord['levelId'] = levelIds[indexRecord['level'].toLowerCase()];
-
-        commands.push(action);
-        commands.push(indexRecord);
-      });
-
-      sails.log.info('Records Prepared');
-
-      // run bulk command
-      await sails.hooks.elastic.client.bulk({body: commands}, (error, response) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(response);
+          _type: index.elasticIndex,
+          _id: indexRecord['id']
         }
       });
-
-
+      commands.push(indexRecord);
     });
+
+    sails.log.info('Records Prepared');
+
+    // run bulk command
+    await sails.hooks.elastic.client.bulk({body: commands})
+      .catch(error => sails.hooks.bugsnag.notify(error));
   }
 };
