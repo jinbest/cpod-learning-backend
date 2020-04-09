fb11ca98561eaaaaf0ee2681a94d2871bfcbbd9b
gumball <ugis.rozkalns@gmail.com>
2020-04-09 15:31:29 +0800

    Randomizing Android Preroll Ads

--- api/controllers/ads/android-app.js
+++ api/controllers/ads/android-app.js
@@ -17,11 +17,40 @@ module.exports = {
   },
 
 
-  fn: function (inputs) {
+  fn: async function (inputs) {
 
     let addIDs = sails.config.custom.prerollAndroidURLs;
 
     let addURL = addIDs[Math.floor(Math.random() * addIDs.length)];
+
+    if (sails.config.environment === 'development') {
+      await sails.helpers.logs.createEvent({
+        'userId': this.req.session.userId ? this.req.session.userId : 'NONE',
+        'email': this.req.me ? this.req.me.email : 'NONE',
+        'sessionId': this.req.session.id,
+        'access_ip': this.req.ip,
+        'action': 'show_mobile_preroll_ad',
+        'event_category': 'advertising',
+        'event_label': `Play Mobile Preroll Ad ${addURL}`,
+        'timestamp': new Date()
+      });
+    } else {
+
+      userInfoQueue.add('LogEvent', {
+        'userId': this.req.session.userId ? this.req.session.userId : 'NONE',
+        'email': this.req.me ? this.req.me.email : 'NONE',
+        'sessionId': this.req.session.id,
+        'access_ip': this.req.ip,
+        'action': 'show_mobile_preroll_ad',
+        'event_category': 'advertising',
+        'event_label': `Play Mobile Preroll Ad ${addURL}`,
+        'timestamp': new Date()
+      },{
+        attempts: 3,
+        timeout: 60000
+      })
+
+    }
 
     const adObject = `<VAST xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="vast.xsd" version="3.0">
     <Ad id="6972004961">
@@ -33,7 +62,7 @@ module.exports = {
             <Creatives>
                 <Creative id="578604590561" sequence="1">
                     <Linear skipoffset="00:00:05">
-                        <Duration>00:03:06</Duration>
+                        <Duration>00:00:15</Duration>
                         <MediaFiles>
                             <MediaFile id="GDFP" delivery="progressive" width="1280" height="720" type="video/mp4" bitrate="533" scalable="true" maintainAspectRatio="true">
                                 <![CDATA[${addURL}]]>
