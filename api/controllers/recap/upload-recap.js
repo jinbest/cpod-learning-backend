d77dcd8be1a54732efe18ec58d7ba5601ff3eedd
gumball <ugis.rozkalns@gmail.com>
2020-04-14 17:10:48 +0800

    Recap File upload sample

--- api/controllers/recap/upload-recap.js
+++ api/controllers/recap/upload-recap.js
@@ -6,13 +6,14 @@ module.exports = {
 
   description: 'Upload recap.',
 
-  files: ['photo'],
+  files: ['files'],
 
   inputs: {
-    label: {
-      type: 'string'
+    lessonId: {
+      type: 'string',
+      required: true
     },
-    photo: {
+    files: {
       description: 'Upstream for an incoming file upload.',
       type: 'ref'
     }
@@ -23,7 +24,7 @@ module.exports = {
   exits: {
 
     success: {
-      outputDescription: 'The newly created `Thing`.',
+      outputDescription: 'The newly uploaded Files',
       outputExample: {}
     },
 
@@ -39,31 +40,28 @@ module.exports = {
   },
 
 
-  fn: async function ({label, photo}) {
+  fn: async function ({lessonId, files}) {
 
-    sails.log.info(this.req.allParams());
-    sails.log.info(this.req.file('photo'));
-
-    var url = require('url');
     var util = require('util');
 
-    const options =
-        {
-          adapter: require('skipper-s3')
-          , key: 'AKIA4DQYSAHG3CJYO4XH'
-          , secret: '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'
-          , bucket: 'chinesepod-recap'
-          , region: 'us-east-1'
-          , dirname: label
-          , ACL: 'public-read',
-          saveAs: label
-        };
+    let options =
+      {
+        adapter: require('skipper-s3')
+        , key: 'AKIA4DQYSAHG3CJYO4XH'
+        , secret: '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'
+        , bucket: 'chinesepod-recap'
+        , region: 'us-east-1'
+        , dirname: lessonId
+        , ACL: 'public-read',
+        saveAs: function (__newFileStream, next) {
+          sails.log.info(__newFileStream.filename);
+          return next(undefined, __newFileStream.filename);
+        }
+      };
 
-
-    let uploadedFiles =  await sails.uploadOne(photo, options)
+    let uploadedFiles = await sails.uploadOne(files, options)
       .intercept('E_EXCEEDS_UPLOAD_LIMIT', 'tooBig')
       .intercept((err)=>new Error('The photo upload failed: '+util.inspect(err)));
-
 
     if(!uploadedFiles) {
       throw 'noFileAttached';
