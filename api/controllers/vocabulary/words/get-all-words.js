8f627ed57e0cfa854884447ae06f95adc6383b67
gumball <ugis.rozkalns@gmail.com>
2020-05-19 11:22:07 +0800

    Expanding Vocab Decks

--- api/controllers/vocabulary/words/get-all-words.js
+++ api/controllers/vocabulary/words/get-all-words.js
@@ -27,7 +27,8 @@ module.exports = {
 
   fn: async function (inputs) {
 
-    inputs.userId = sails.config.environment === 'development' ? 1016995 : this.req.session.userId;
+    // inputs.userId = sails.config.environment === 'development' ? 1016995 : this.req.session.userId;
+    inputs.userId = sails.config.environment === 'development' ? 1089814 : this.req.session.userId;
 
     let total = await UserVocabulary.count({user_id: inputs.userId});
 
@@ -38,13 +39,17 @@ module.exports = {
     return {
       total: total,
       vocabulary: userVocab.map(vocab => {
-        let tags = userDecks.filter(deck => deck.user_vocabulary_id === vocab.id);
-        if (tags && tags.length) {
-          vocab.vocabulary_id.tags = tags.map(tag => tag.vocabulary_tag_id)
+        if (vocab.vocabulary_id) {
+          let tags = userDecks.filter(deck => deck.user_vocabulary_id === vocab.id);
+          if (tags && tags.length) {
+            vocab.vocabulary_id.tags = tags.map(tag => tag.vocabulary_tag_id)
+          }
+          vocab.lesson = _.pick(vocab.vocabulary_id.v3_id, ['title', 'hash_code', 'slug', 'level', 'type', 'id']);
+          delete vocab.vocabulary_id.v3_id
+          return {...vocab.vocabulary_id, ...{user_vocabulary_id: vocab.id, createdAt: vocab.createdAt, lesson: vocab.lesson}}
+        } else {
+          sails.log.info(vocab)
         }
-        vocab.lesson = _.pick(vocab.vocabulary_id.v3_id, ['title', 'hash_code', 'slug', 'level', 'type', 'id']);
-        delete vocab.vocabulary_id.v3_id
-        return {...vocab.vocabulary_id, ...{user_vocabulary_id: vocab.id, createdAt: vocab.createdAt, lesson: vocab.lesson}}
       })
     }
   }
