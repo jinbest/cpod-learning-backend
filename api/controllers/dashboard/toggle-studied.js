64b14a1cc98a8a4f00b992e870de4f5e20e1cf6b
gumball <ugis.rozkalns@gmail.com>
2020-05-29 16:02:20 +0800

    Adding Latest Lesson Studied tracking

--- api/controllers/dashboard/toggle-studied.js
+++ api/controllers/dashboard/toggle-studied.js
@@ -31,23 +31,17 @@ module.exports = {
   fn: async function (inputs) {
     inputs.userId = sails.config.environment === 'development' ? 1016995 : this.req.session.userId;
 
-    let currentStatus = await UserContents.find({user_id: inputs.userId, lesson: inputs.lessonId, lesson_type: 0});
-
-    if (currentStatus && currentStatus.length > 0) {
-
-      return await UserContents.update({id: {in: currentStatus.map(lesson => lesson.id)}})
-        .set({studied: inputs.status ? 1 : 0})
-
-    } else {
-
-      return await UserContents.create({
-        user_id: inputs.userId,
-        lesson: inputs.lessonId,
-        studied: inputs.status ? 1 : 0,
-        lesson_type: 0
-      })
-
+    if (inputs.status) {
+      await UserOptions.updateOrCreate(
+        {user_id: inputs.userId, option_key: 'lastStudiedLesson'},
+        {user_id: inputs.userId, option_key: 'lastStudiedLesson', option_value: inputs.lessonId }
+      )
     }
+
+    return await UserContents.updateOrCreate(
+      {user_id: inputs.userId, lesson: inputs.lessonId, lesson_type: 0},
+      {user_id: inputs.userId, lesson: inputs.lessonId, studied: inputs.status ? 1 : 0, lesson_type: 0}
+    )
 
   }
 
