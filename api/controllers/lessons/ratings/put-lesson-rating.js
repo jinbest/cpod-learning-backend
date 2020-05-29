26180eddfce550c54607967930172dd5f25d1f99
gumball <ugis.rozkalns@gmail.com>
2020-05-29 15:11:31 +0800

    Adding Lesson Reviews

--- api/controllers/lessons/ratings/put-lesson-rating.js
+++ api/controllers/lessons/ratings/put-lesson-rating.js
@@ -8,19 +8,41 @@ module.exports = {
 
 
   inputs: {
+    slug: {
+      type: 'string',
+      required: true
+    },
+    rating: {
+      type: 'number',
+      required: true
+    },
+    teachingNotification: {
+      type: 'boolean'
+    }
 
   },
 
 
   exits: {
+    invalid: {
+      responseType: 'badRequest'
+    }
 
   },
 
 
   fn: async function (inputs) {
+    inputs.userId = sails.config.environment === 'development' ? 1016995 : this.req.session.userId;
 
-    // All done.
-    return;
+    let lessonData = await LessonData.findOne({slug: inputs.slug})
+
+    if(!lessonData) {
+      throw 'invalid'
+    }
+
+    return await ContentRates.updateOrCreate(
+      {user_id: inputs.userId, lessonId: lessonData.id},
+      {user_id: inputs.userId, lessonId: lessonData.id, rate: inputs.rating})
 
   }
 
