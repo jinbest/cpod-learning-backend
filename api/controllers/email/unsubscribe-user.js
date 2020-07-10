768c51a4dfa0a429684cb3c43bb4e193d7c8bd6e
gumball <ugis.rozkalns@gmail.com>
2020-07-10 16:35:20 +0800

    Updated Unsubscribe page

--- api/controllers/email/unsubscribe-user.js
+++ api/controllers/email/unsubscribe-user.js
@@ -8,6 +8,15 @@ module.exports = {
 
 
   inputs: {
+    unsubscribeAll: {
+      type: 'boolean'
+    },
+    options: {
+      type: {}
+    },
+    preferences: {
+      type: {},
+    },
 
   },
 
@@ -27,15 +36,50 @@ module.exports = {
       inputs.userId = this.req.session.limitedAuth.id
     }
 
-    await MailingDoNotContact.updateOrCreate({user_id: inputs.userId}, {
-      user_id: inputs.userId,
-      reason: 'Website Unsubscribe Button'
-    });
+    if (inputs.unsubscribeAll) {
 
-    // All done.
-    return;
+      await MailingDoNotContact.updateOrCreate({user_id: inputs.userId}, {
+        user_id: inputs.userId,
+        reason: 'Website Unsubscribe Button'
+      });
+
+    } else if (inputs.options && inputs.preferences) {
+
+      let promises = [];
+      promises.push(
+        UserOptions.updateOrCreate(
+        {user_id: inputs.userId, option_key: 'emailPreferences'},
+        {user_id: inputs.userId, option_key: 'emailPreferences', option_value: JSON.stringify(inputs.options)}
+        )
+      );
+
+      promises.push(
+        UserOptions.updateOrCreate(
+          {user_id: inputs.userId, option_key: 'levelInterests'},
+          {user_id: inputs.userId, option_key: 'levelInterests', option_value: JSON.stringify(inputs.preferences.levelInterests)}
+        )
+      );
+
+      promises.push(
+        UserOptions.updateOrCreate(
+          {user_id: inputs.userId, option_key: 'charSet'},
+          {user_id: inputs.userId, option_key: 'charSet', option_value: inputs.preferences.charSet}
+        )
+      );
+
+      if(inputs.preferences.hasOwnProperty('notInterestedInCharacters')) {
+        promises.push(
+          UserOptions.updateOrCreate(
+            {user_id: inputs.userId, option_key: 'notInterestedInCharacters'},
+            {user_id: inputs.userId, option_key: 'notInterestedInCharacters', option_value: inputs.preferences.notInterestedInCharacters}
+          )
+        );
+      }
+
+      await Promise.all(promises);
+
+    }
 
   }
-
 
 };
