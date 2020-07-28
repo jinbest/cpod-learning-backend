4a19b81e9f6d532092511f86a137fdf708a5c0aa
gumball <ugis.rozkalns@gmail.com>
2020-07-28 15:35:21 +0800

    YT Promo Logic Expansion

--- api/controllers/youtube/put-confirmation.js
+++ api/controllers/youtube/put-confirmation.js
@@ -32,19 +32,44 @@ module.exports = {
 
     let userContest = await UserOptions.findOne({id: inputs.id, option_key: 'youtubeContest'});
 
-    if (userContest && userContest.option_value === 'initial') {
-      await UserSiteLinks.updateOrCreate(
-        {user_id: userContest.user_id, site_id: 2},
-        {user_id: userContest.user_id, site_id: 2, usertype_id: 5, expiry: moment().add(60, 'days').toDate()}
-        )
+    if (userContest) { // && userContest.option_value === 'initial'
 
-      return await UserOptions.updateOne({id: userContest.id}).set({option_value: 'used'});
+      await UserOptions.updateOne({id: userContest.id}).set({option_value: 'used'});
+
+      let userData = await User.updateOne({id: userContest.user_id})
+        .set({code: await sails.helpers.strings.random('url-friendly')});
+
+      let voucher;
+      while (!voucher) {
+        voucher = await Vouchers.create({
+          voucher_code: await sails.helpers.strings.random('url-friendly').slice(0,10).toUpperCase(),
+          product_id: 18,  // 90 Day Premium Plan
+          expiry_date: moment().add(30, 'days').toDate(),
+        })
+          .intercept('E_UNIQUE', () => {sails.hooks.bugsnag.notify('Voucher already exists')})
+          .fetch();
+      }
+
+      let firstName = await sails.helpers.users.calculateFirstName(userData.id);
+
+      await sails.helpers.sendTemplateEmail.with({
+        to: userData.email,
+        subject: `Your 90 Day Premium Pass!`,
+        template: 'automated/email-gift',
+        layout: false,
+        toName: userData.name,
+        templateData: {
+          firstName: firstName ? firstName : 'Learner',
+          claimLink: `https://www.chinesepod.com/redeem-gift/${voucher.voucher_code}/${userData.code}`
+        }
+      });
+
+      return;
 
     }
 
     throw 'invalid'
 
   }
-
 
 };
